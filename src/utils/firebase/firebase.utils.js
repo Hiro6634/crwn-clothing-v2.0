f40f5fcbb6b0cf84afd28fb2bd1c46ed88a5fed9
Hiro6634 <hiro.suyama@gamil.com>
2023-03-26 13:12:27 -0300

    Sign Up with email + password

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,9 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import {
     getAuth,
-    signInWithRedirect,
     signInWithPopup,
-    GoogleAuthProvider
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword
 } from 'firebase/auth';
 
 import {
@@ -40,7 +40,10 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+    userAuth ,
+    additionalInformation = {}
+    ) => {
     const userDocRef = doc( db, 'users', userAuth.uid);
 
     console.log(userDocRef);
@@ -58,7 +61,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
             await setDoc( userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation
             });
         } catch(error){
             console.log('Error creating the user', error.message);
@@ -66,4 +70,10 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     }
 
     return userDocRef;
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await createUserWithEmailAndPassword( auth, email, password);
 }
