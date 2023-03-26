4420808b02b86b16e64205b8b77bccbcb9fa20a0
Hiro6634 <hiro.suyama@gamil.com>
2023-03-25 21:28:28 -0300

    React Router Link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 
 
@@ -11,7 +12,8 @@ const App = () => {
   }
   return (
     <Routes>
-      <Route path='/home' element={<Home/>}>
+      <Route path='/' element={<Navigation/>}>
+        <Route index element={<Home/>}/>
         <Route path='shop' element={<Shop/>}/>
       </Route>
     </Routes>
