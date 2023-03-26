efd4a595b56b23ea3524ef64855538ebfd980930
Hiro6634 <hiro.suyama@gamil.com>
2023-03-26 00:11:01 -0300

    Styling for Navigation + Logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,19 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
 
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
+
+
 const Navigation = () => {
     return (
       <Fragment>
-        <div className="navigattion">
+        <div className="navigation">
           <Link className='logo-container' to='/'>
-              <div>Logo</div>
+              <CrwnLogo className="logo"/>
           </Link>
           <div className="nav-links-container">
-            <Link className='nav-link-container' to='/shop'>
+            <Link className='nav-link' to='/shop'>
                 SHOP
             </Link>
           </div>
